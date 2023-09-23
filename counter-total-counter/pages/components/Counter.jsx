1829749969d6e33765daba71f89dd3b67b533b55
Aluon <paulacanoroman@gmail.com>
2023-09-23 21:50:58 +0200

    component Counter con botones

---
+++ counter-total-counter/pages/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Counter = () => {
+
+
+
+  return (
+    <>
+    <button onClick={()=>{}}>+</button>
+    <button onClick={()=>{}}>-</button>
+    </>
+  )
+}
+
+export default Counter
