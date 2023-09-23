c8c0eeeabef83112f7333646c473e97237c93f82
Aluon <paulacanoroman@gmail.com>
2023-09-23 21:53:32 +0200

    Contador funcional

--- counter-total-counter/pages/components/Counter.jsx
+++ counter-total-counter/pages/components/Counter.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Counter = () => {
 
+    const [calculoContador, setCalculoContador] = useState(0)
 
 
   return (
     <>
-    <button onClick={()=>{}}>+</button>
-    <button onClick={()=>{}}>-</button>
+    {calculoContador}
+    <button onClick={()=>{setCalculoContador(calculoContador + 1)}}>+</button>
+    <button onClick={()=>{setCalculoContador(calculoContador - 1)}}>-</button>
     </>
   )
 }
