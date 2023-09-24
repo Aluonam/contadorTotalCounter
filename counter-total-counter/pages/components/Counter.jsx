7dca17ec57840b2ecb4c23bff9376cb6312f4f96
Aluon <paulacanoroman@gmail.com>
2023-09-24 23:41:35 +0200

    hora local

--- counter-total-counter/pages/components/Counter.jsx
+++ counter-total-counter/pages/components/Counter.jsx
@@ -1,17 +1,26 @@
 import React, {useState} from 'react'
 import estiloCSS from '@/pages/components/Counter.module.css'
 
-const Counter = ({calculoContador,setCalculoContador}) => {
+const Counter = ({setAllCounter}) => {
 
-    
 
+    const [calculoContador, setCalculoContador] = useState(0)
+
+
+    const sumar = ()=>{
+        const contador1 = setCalculoContador(calculoContador + 1)
+        setAllCounter(contador1)
+    }
+    const restar = ()=>{
+        const contador1 = setCalculoContador(calculoContador - 1)
+        setAllCounter(contador1)
+    }
 
   return (
     <>
-    
-    <button onClick={()=>{setCalculoContador(calculoContador + 1)}}
+    <button onClick={()=>{sumar()}}
         className={estiloCSS.botonSumar}>+</button>
-    <button onClick={()=>{setCalculoContador(calculoContador - 1)}}
+    <button onClick={()=>{restar()}}
         className={estiloCSS.botonRestar}>-</button>
     </>
   )
